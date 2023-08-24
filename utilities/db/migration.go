6bbab8dfa86270a61d6f5ce0cbcdde6e654b1e70
Ahmad Mustapha <18-30gc052@students.unilorin.edu.ng>
2023-08-24 12:52:12 +0100

    Added models to auto migrate

--- utilities/db/migration.go
+++ utilities/db/migration.go
@@ -1,8 +1,18 @@
 package db
 
-import "github.com/jinzhu/gorm"
+import (
+	
+
+	"github.com/Smylet/symlet-backend/api/hostel"
+	"github.com/Smylet/symlet-backend/api/student"
+	"gorm.io/gorm"
+)
 
 func Migrate(db *gorm.DB) {
-	db.AutoMigrate(GetModels())
+	db.AutoMigrate(
+		student.Student{},
+		hostel.Hostel{},
+		hostel.HostelStudent{},
+	)
 }
 
