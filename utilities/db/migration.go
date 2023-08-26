ca0a6abf8916b48532b7c917c9dd1ac8db749085
r-sc <abdulrahmanolamilekan88@gmail.com>
2023-08-26 22:15:02 +0100

    Added User/Profile to AutoMigrate

--- utilities/db/migration.go
+++ utilities/db/migration.go
@@ -3,11 +3,14 @@ package db
 import (
 	"github.com/Smylet/symlet-backend/api/hostel"
 	"github.com/Smylet/symlet-backend/api/student"
+	"github.com/Smylet/symlet-backend/api/users"
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) {
 	err := db.AutoMigrate(
+		users.User{},
+		users.Profile{},
 		student.Student{},
 		hostel.Hostel{},
 		hostel.HostelStudent{},
