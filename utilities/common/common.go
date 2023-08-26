8f0d3ef6b6eefd73d75bd205c529822b97f5f38a
r-sc <abdulrahmanolamilekan88@gmail.com>
2023-08-26 22:18:37 +0100

    Added general app utilities

---
+++ utilities/common/common.go
@@ -0,0 +1,19 @@
+package common
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type ModelInterface interface {
+	isModel() bool
+}
+
+type AbstractBaseModel struct {
+	gorm.Model
+	UID uuid.UUID `pg:"type:uuid"`
+}
+
+func (m *AbstractBaseModel) isModel() bool {
+	return true
+}
