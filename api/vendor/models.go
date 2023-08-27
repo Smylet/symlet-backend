1d23fdf6a92a8dd947d917a88a68162737b9f842
Ahmad Mustapha <18-30gc052@students.unilorin.edu.ng>
2023-08-27 14:17:29 +0100

    added vendor model

---
+++ api/vendor/models.go
@@ -0,0 +1,21 @@
+package vendor
+
+import (
+	"github.com/Smylet/symlet-backend/api/core"
+)
+
+type Vendor struct {
+	core.AbstractBaseModel
+	UserID uint `gorm:"not null"`
+	CompanyName string `gorm:"not null"`
+	Address string `gorm:"not null"`
+	Email string `gorm:"not null"`
+	Phone string `gorm:"not null"`
+	Website string `gorm:"not null"`
+	Logo string `gorm:"not null"`
+	Description string `gorm:"not null"`
+	Service string `gorm:"not null"`
+	Rating float64 `gorm:"not null"`
+	Reviews []VendorReview `gorm:"foreignKey:VendorID"`
+	IsVerified bool `gorm:"default:false"`
+}
