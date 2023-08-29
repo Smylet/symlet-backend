26a83d93b403da70321646416d196660d706546c
Ahmad Mustapha <18-30gc052@students.unilorin.edu.ng>
2023-08-29 21:59:52 +0100

    Added migration file

--- utilities/db/migration.go
+++ utilities/db/migration.go
@@ -1,16 +1,49 @@
 package db
 
 import (
+	"github.com/Smylet/symlet-backend/api/booking"
 	"github.com/Smylet/symlet-backend/api/hostel"
+	"github.com/Smylet/symlet-backend/api/maintenance"
+	"github.com/Smylet/symlet-backend/api/notification"
+	"github.com/Smylet/symlet-backend/api/reference"
+	"github.com/Smylet/symlet-backend/api/review"
 	"github.com/Smylet/symlet-backend/api/student"
+	"github.com/Smylet/symlet-backend/api/vendor"
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) {
 	err := db.AutoMigrate(
+		reference.ReferenceHostelAmmenities{},
+		reference.ReferenceUniversity{},
 		student.Student{},
+		//hostel
 		hostel.Hostel{},
-		hostel.HostelStudent{},
+		booking.HostelStudent{},
+		booking.HostelBooking{},
+		booking.CustomPaymentPlan{},
+		booking.PaymentDistribution{},
+		hostel.HostelImage{},
+		hostel.HostelFee{},
+		hostel.HostelAgreementTemplate{},
+
+		//maintenance
+		maintenance.HostelMaintenanceRequest{},
+		maintenance.HostelMaintenanceRequestImage{},
+		maintenance.WorkOrder{},
+		maintenance.WorkOrderImage{},
+		maintenance.WorkOrderComment{},
+
+		//notification
+		notification.Notification{},
+
+		//vendor
+		vendor.Vendor{},
+
+		//Review
+		review.HostelReview{},
+		review.HostelManagerReview{},
+		review.VendorReview{},
 	)
 	if err != nil {
 		panic(err)
