b69b30cac93f22ce833068600a4d070545cba0a4
r-sc <abdulrahmanolamilekan88@gmail.com>
2023-09-02 01:03:16 +0100

    Added update func/VerifyEmailTx

--- api/users/model.go
+++ api/users/model.go
@@ -4,7 +4,6 @@ package users
 import (
 	"context"
 	"fmt"
-	"reflect"
 	"time"
 
 	"github.com/Smylet/symlet-backend/utilities/common"
@@ -13,13 +12,13 @@ import (
 
 type User struct {
 	common.AbstractBaseModel
-	ID        uint `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Username  string `gorm:"unique;not null"`
-	Email     string `gorm:"unique;not null"`
-	Password  string `sql:"not null"`
-	Profile   Profile
+	ID                 uint `gorm:"primary_key"`
+	CreatedAt          time.Time
+	UpdatedAt          time.Time
+	Username           string `gorm:"unique;not null"`
+	Email              string `gorm:"unique;not null"`
+	Password           string `sql:"not null"`
+	Is_email_confirmed bool   `gorm:"default:false"`
 }
 
 type Profile struct {
@@ -29,11 +28,12 @@ type Profile struct {
 	Image  string
 }
 
-type VerifyEmail struct {
+type VerificationEmail struct {
 	ID         uint `gorm:"primary_key"`
 	Email      string
 	SecretCode string
 	ExpiresAt  time.Time
+	UserID     uint
 }
 
 func CreateUserTx(ctx context.Context, database *gorm.DB, arg CreateUserTxParams) (CreateUserTxResult, error) {
@@ -76,25 +76,11 @@ type FindUserParams struct {
 func FindUser(ctx context.Context, database *gorm.DB, arg FindUserParams) (UserSerializer, error) {
 	var result UserSerializer
 
-	query := database.WithContext(ctx).Model(&User{})
+	query := buildQueryFromUser(ctx, database, Args{User: arg.User})
 
-	v := reflect.ValueOf(arg.User)
-	t := reflect.TypeOf(arg.User)
-	for i := 0; i < v.NumField(); i++ {
-		field := v.Field(i)
-		fieldType := t.Field(i)
-
-		// Only process non-zero fields.
-		if !field.IsZero() {
-			// Assumes that the column name in the DB is same as struct field name.
-			// If they're different, you'd use gorm struct tags to get DB column names.
-			query = query.Where(fmt.Sprintf("%s = ?", fieldType.Name), field.Interface())
-		}
-	}
-
-	if arg.IncludeProfile {
-		query = query.Preload("Profile")
-	}
+	// if arg.IncludeProfile {
+	// 	query = query.Preload("Profile")
+	// }
 
 	if err := query.First(&result.User).Error; err != nil {
 		return UserSerializer{}, fmt.Errorf("failed to get user: %w", err)
@@ -103,11 +89,37 @@ func FindUser(ctx context.Context, database *gorm.DB, arg FindUserParams) (UserS
 	return result, nil
 }
 
-func CreateVerifyEmail(ctx context.Context, database *gorm.DB, req CreateVerifyEmailParams) (VerifyEmail, error) {
-	verifyEmail := VerifyEmail{
+type UpdateUserParams struct {
+	Criteria User // Fields to search for the user
+	Updates  User // Fields to update for the user
+}
+
+func UpdateUser(ctx context.Context, database *gorm.DB, arg UpdateUserParams) (UserSerializer, error) {
+	var result UserSerializer
+
+	// Use the buildQueryFromUser function to get a query based on non-zero fields.
+	query := buildQueryFromUser(ctx, database, Args{User: arg.Criteria})
+
+	arg.Updates.UpdatedAt = time.Now()
+	query = query.Updates(arg.Updates)
+
+	// Execute the updates based on the built query.
+
+	if err := query.First(&result.User).Error; err != nil {
+		return UserSerializer{}, fmt.Errorf("failed to retrieve updated user: %w", err)
+	}
+
+	return result, nil
+}
+
+var ExpiryTime = time.Hour * 24
+
+func CreateVerifyEmail(ctx context.Context, database *gorm.DB, req CreateVerifyEmailParams) (VerificationEmail, error) {
+	verifyEmail := VerificationEmail{
+		UserID:     req.UserID,
 		Email:      req.Email,
 		SecretCode: req.SecretCode,
-		ExpiresAt:  time.Now().Add(time.Hour * 24),
+		ExpiresAt:  time.Now().Add(ExpiryTime),
 	}
 
 	if err := database.Create(&verifyEmail).Error; err != nil {
@@ -117,41 +129,37 @@ func CreateVerifyEmail(ctx context.Context, database *gorm.DB, req CreateVerifyE
 	return verifyEmail, nil
 }
 
-func UpdateVerifyEmail(ctx context.Context, database *gorm.DB, req UpdateVerifyEmailParams) (VerifyEmail, error) {
-	var verifyEmail VerifyEmail
-	if err := database.Where("email = ?", req.Email).First(&verifyEmail).Error; err != nil {
-		return verifyEmail, err
-	}
+func VerifyEmailTx(ctx context.Context, database *gorm.DB, arg ConfirmVerifyEmailParams) error {
+	return common.ExecTx(ctx, database, func(tx *gorm.DB) error {
+		var verifyEmail VerificationEmail
 
-	verifyEmail.SecretCode = req.SecretCode
+		// Chain the query conditions and fetch the record in one operation
+		if err := tx.Model(&VerificationEmail{}).
+			Where("user_id = ? AND id = ?", arg.UserID, arg.VerEmailID).
+			First(&verifyEmail).Error; err != nil {
+			return err
+		}
 
-	if err := database.Save(&verifyEmail).Error; err != nil {
-		return verifyEmail, err
-	}
+		// Check the secret code and expiration
+		if verifyEmail.SecretCode != arg.SecretCode {
+			return fmt.Errorf("invalid secret code")
+		}
+		if verifyEmail.ExpiresAt.Before(time.Now()) {
+			return fmt.Errorf("expired secret code")
+		}
 
-	return verifyEmail, nil
+		// Update the user's email confirmation status
+		if err := tx.Model(&User{}).
+			Where("id = ?", arg.UserID).
+			Update("is_email_confirmed", true).Error; err != nil {
+			return err
+		}
+
+		// Delete the verification record
+		if err := tx.Delete(&verifyEmail).Error; err != nil {
+			return err
+		}
+
+		return nil
+	})
 }
-
-// type VerifyEmailTxParams struct {
-// 	EmailId    int64
-// 	SecretCode string
-// }
-
-// type VerifyEmailTxResult struct {
-// 	User User
-// 	err  error // Add an error field of type error
-// }
-
-// func VerifyEmailTx(ctx context.Context, database *gorm.DB, arg VerifyEmailTxParams) (VerifyEmailTxResult, error) {
-// 	var result VerifyEmailTxResult
-
-// 	err := common.ExecTx(ctx, database, func(tx *gorm.DB) error {
-
-// 		return nil
-// 	})
-// 	if err != nil {
-// 		return VerifyEmailTxResult{}, fmt.Errorf("transaction error: %v", err)
-// 	}
-
-// 	return result, nil
-// }
