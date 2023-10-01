457f6c52e0ea93c8904849897c52a4242fc25d97
r-sc <abdulrahmanolamilekan88@gmail.com>
2023-10-01 10:44:31 +0100

    updates

--- tests/integration/users/create_user_test.go
+++ tests/integration/users/create_user_test.go
@@ -36,7 +36,7 @@ func (s *CreateUserTestSuite) SetupSuite() {
 
 	db, err := db.GetDB(config)
 	if err != nil {
-		log.Fatal().Err(err).Msg("Error establishing database connection: ", err)
+		log.Fatal().Msg("Error establishing database connection: ", err)
 		return // No need to continue
 	}
 	serviceUrl := config.HTTPServerAddress
